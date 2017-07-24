d3485b6003eb13476ef1dda77a283fa4a5a0ba19
tom <tom@sears.co.il>
2017-07-24 15:21:33 +0300

    Change VersionInfo

--- Source/MySql.Data/Properties/VersionInfo.cs
+++ Source/MySql.Data/Properties/VersionInfo.cs
@@ -38,6 +38,6 @@ using System.Resources;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("6.9.9")]
-[assembly: AssemblyInformationalVersion("6.9.9")]
+[assembly: AssemblyVersion("6.9.100")]
+[assembly: AssemblyInformationalVersion("6.9.100")]
 [assembly: NeutralResourcesLanguage("en-US")]
